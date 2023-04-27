a63a1424db6998f03e3527d69cddaa6638e0dda3
sipra <sipramishra085@gmail.com>
2023-04-27 15:47:52 +0530

    Added Main Program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+           InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+           double fare = invoiceGenerator.CalculateFare(2.0, 5);
+
+            Console.WriteLine(fare);
+
+            //int val = (int)Month.Feb;
+            //Console.WriteLine(val);
+        }
+
+        //enum Month
+        //{
+        //    Jan=45,
+        //    Feb=67,
+        //    Mar
+        //}
+    }
+}
