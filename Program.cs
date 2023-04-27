712a0f8d558e82e76ff472d660bc5be17e93c6eb
sipra <sipramishra085@gmail.com>
2023-04-27 16:21:12 +0530

    added for uc2

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            {
+           
                 Console.WriteLine("Welcome to Cab Invoice Generator");
                 InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
 
                 double fare = invoiceGenerator.CalculateFare(2.0, 5);
 
                 Console.WriteLine(fare);
-            }
-           
+            
+
         }
 
        
