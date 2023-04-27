b7be76a1be675ae82f0bee5ff9acf3ccb32c0fc4
sipra <sipramishra085@gmail.com>
2023-04-27 16:07:17 +0530

    added program for uc1

--- Program.cs
+++ Program.cs
@@ -4,22 +4,17 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Cab Invoice Generator");
-           InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            {
+                Console.WriteLine("Welcome to Cab Invoice Generator");
+                InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
 
-           double fare = invoiceGenerator.CalculateFare(2.0, 5);
+                double fare = invoiceGenerator.CalculateFare(2.0, 5);
 
-            Console.WriteLine(fare);
-
-            //int val = (int)Month.Feb;
-            //Console.WriteLine(val);
+                Console.WriteLine(fare);
+            }
+           
         }
 
-        //enum Month
-        //{
-        //    Jan=45,
-        //    Feb=67,
-        //    Mar
-        //}
+       
     }
 }
